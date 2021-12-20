875d16e9eb2d550ffce8d9546b6091af323fa16a
NNikolov02 <>
2021-12-20 17:14:03 +0000

    Ubung2

--- src/exercise2/QuadraticEquation.java
+++ src/exercise2/QuadraticEquation.java
@@ -10,37 +10,31 @@ public class QuadraticEquation
   public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-    float a = input.nextFloat();
-    float b = input.nextFloat();
-    float c = input.nextFloat();
-    double D = (b * b) - (4.0 * a * c);
-    double x1;
-    double x2;
-    float x;
- 
- if(a != 0){
-   if(D >= 0){
-     x1=((b * - 1) + Math.sqrt(D)) / (2 * a);
-     x2=((b * - 1) - Math.sqrt(D)) / (2 * a);
-     System.out.println(x1);
-     System.out.println(x2);
-   } else {
-     System.out.println("Imaginary values");
-   }
-   } else {
-     if(b != 0){
-       x = (0 - c) / b;
-       System.out.println(x);
-     } else {
-       if(c != 0){
-         System.out.println("No values");
-       } else { 
-         System.out.println("Many values");
-       }
+   double a = input.nextDouble();
+    double b = input.nextDouble();
+    double c = input.nextInt();
+    double D = b*b - 4.0*a*c;
+    double x1 = ((b* -1.0) + Math.sqrt(D))/2.0*a;
+    double x2 = ((b* -1.0) - Math.sqrt(D))/2.0*a;
+    double x = (c* -1.0)/b;
+    
+
+    if (a != 0) {
+      if (D >= 0) {
+        System.out.println(x1 + ", " + x2);
+      } else {
+        System.out.println("Imaginary values");    
+          } 
+          } else {
+            if (b != 0) {
+              System.out.println(x);
+            } else {
+              if (c != 0) {
+                System.out.println("No values");
+              } else {
+                System.out.println("Many values");
+        }
+      }
      }
-   }
-  }
-}
-  
-   
-   
+   } 
+ } 
